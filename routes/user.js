b07ec87b59968f8a24c890d60c186ae05f9cceab
knwpound <142076222+knwpound@users.noreply.github.com>
2025-04-18 23:22:41 +0700

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,6 @@ const router = express.Router();
 const {protect,authorize} = require('../middleware/auth')
 
 router.route('/').get(getUsers);
-router.route('/:id').get(getUser).put(protect,authorize('admin','user'),putUser).delete(protect,authorize('admin'),deleteUser);
+router.route('/:id').get(getUser).put(protect,authorize('admin','user'),putUser).delete(protect,authorize('admin','user'),deleteUser);
 
 module.exports = router;
